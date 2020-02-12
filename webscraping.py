d002da7e5abc2b9cb185c76c79ef93bc8d69f2e9
JoJunYeong <you@example.com>
2020-02-12 23:55:02 +0900

    fileupdate

---
+++ webscraping.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe',options=options)
+driver.get("http://ncov.mohw.go.kr/index_main.jsp")
+
+path = '/html/body/div/div[5]/div/div[1]/div[1]'
+table = driver.find_element_by_xpath(path)
+
+print(table)
