f92b3d860db9de179bad820771efaf2f3f6def35
JoJunYeong <you@example.com>
2020-02-15 18:18:40 +0900

    update

--- webscraping.py
+++ webscraping.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
-driver = webdriver.Chrome('C:\chromedriver\chromedriver.exe',options=options)
+driver = webdriver.Chrome('C:\chromedriver',options=options)
 driver.get("http://ncov.mohw.go.kr/index_main.jsp")
 
 path = '/html/body/div/div[5]/div/div[1]/div[1]'
