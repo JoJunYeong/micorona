c0a0911633421499e560ac5319b53097239b4bf4
leesowoon <61323319+leesowoon@users.noreply.github.com>
2020-02-24 11:10:28 +0900

    변수로 저장

--- micorona_ejs/app.js
+++ micorona_ejs/app.js
@@ -7,14 +7,12 @@ var app = express();
 
 
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // set path for static assets
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // routes
 app.use('/', index);
