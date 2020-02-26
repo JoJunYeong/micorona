a596097ac0d3d04a9b0d4b19701647cf7da41fb5
JoJunYeong <you@example.com>
2020-02-27 03:45:13 +0900

    ud

--- micorona_ejs/app.js
+++ micorona_ejs/app.js
@@ -5,14 +5,13 @@ var index = require('./routes/index');
 var app = express();
 
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // set path for static assets
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // routes
 app.use('/', index);
@@ -31,17 +30,38 @@ app.use(function(err, req, res, next) {
   res.render('error', {status:err.status, message:err.message});
 });
 
-app.listen(8000,function(){
-  console.log("heard on 8000");
-
-});
-
-app.get('/', function(request,response){
-  response.render('index', {
-    foo : 'bar'
-
-  });
-});
-
-
 module.exports = app;
+
+//크롤링
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+app.get('/getKoreaData', function(req, res){
+
+	console.log("Success GET "+req.query.data);
+
+	async function getHTML() {
+		try {
+			return await axios.get("http://ncov.mohw.go.kr/index_main.jsp");
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	getHTML()
+	.then(html => {
+		let numList = [];
+		const $ = cheerio.load(html.data);
+		const bodyList = $(".co_cur .num")
+		bodyList.each(function(i, elem) {
+			numList[i] = $(this).text().replace(/[^0-9]/g,"");
+		});
+		return numList;
+	}).then(result => {
+		console.log(result);
+ 	// kor = copyObject(result);
+ 	res.send({result:result});
+ });
+
+});
+
--- micorona_ejs/routes/index.js
+++ micorona_ejs/routes/index.js
@@ -1,29 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
-
-
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-      page:'지도-내 주변 확진자 동선',
-      menuId:'home',
-      foo:'bar'
-  });
+  res.render('index', {page:'지도-내 주변 확진자 동선', menuId:'home'});
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {page:'소개 및 유용한 사이트', menuId:'about'});
+ router.get('/about', function(req, res, next) {
+ res.render('about', {page:'소개 및 유용한 사이트', menuId:'about'});
 });
 
-router.get('/datas', function(req, res, next) {
-  res.render('datas', {page:'데이터-전세계 코로나 바이러스 추이', menuId:'datas'});
-});
 
 router.get('/test', function(req, res, next){
 	res.render('test', {page:'Test', menuId:'test'});
 });
+
+router.get('/datas', function(req, res, next) {
+	res.render('datas', {page:'데이터-전세계 코로나 바이러스 추이', menuId:'datas'
+	});
+});
+
+//크롤링
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+router.get('/getKoreaData', function(req, res, next){
+
+	console.log("Success GET "+req.query.data);
+
+	async function getHTML() {
+		try {
+			return await axios.get("http://ncov.mohw.go.kr/index_main.jsp");
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	getHTML()
+	.then(html => {
+		let numList = [];
+		const $ = cheerio.load(html.data);
+		const bodyList = $(".co_cur .num")
+		bodyList.each(function(i, elem) {
+			numList[i] = $(this).text().replace(/[^0-9]/g,"");
+		});
+		return numList;
+	}).then(result => {
+		// console.log(result);
+ 	// kor = copyObject(result);
+ 	res.send({result:result});
+ });
+
+});
+
 
 module.exports = router;
